e28c05b5749c7cf6d76fc9e7ffb1b353f8d36180
Brian Schaper <briansc@gmail.com>
2018-04-02 23:26:20 -0700

    updated login component

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,5 +1,5 @@
 import {Component, OnInit} from '@angular/core';
-import {AuthenticationService, TokenPayload} from '../../services/authentication.service';
+import {AuthenticationService, TokenPayload} from '../../shared/services/authentication.service';
 import {Router} from '@angular/router';
 
 @Component({
