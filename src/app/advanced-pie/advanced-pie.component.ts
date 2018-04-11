13173d72cad2bc9c6e8b425a9c61adb4624d97b3
Brian Schaper <brian.schaper@little-bamboo.com>
2018-04-11 10:53:12 -0700

    added advanced pie chart

---
+++ src/app/advanced-pie/advanced-pie.component.ts
@@ -0,0 +1,31 @@
+import {Component, OnInit} from '@angular/core';
+import {single, multi} from './data';
+
+
+@Component({
+  selector: 'app-advanced-pie',
+  templateUrl: './advanced-pie.component.html',
+  styleUrls: ['./advanced-pie.component.scss']
+})
+export class AdvancedPieComponent implements OnInit {
+
+  single: any[];
+  multi: any[];
+  view: any[] = [700, 400];
+
+  colorScheme = {
+    domain: ['#5AA454', '#A10A28', '#C7B42C', '#AAAAAA']
+  };
+
+  constructor() {
+    Object.assign(this, {single, multi});
+  }
+
+  ngOnInit() {
+  }
+
+  onSelect(event) {
+    console.log(event);
+  }
+
+}
